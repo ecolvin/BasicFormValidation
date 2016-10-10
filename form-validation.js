b0d92fec6d59958822a73bbe82321bb8f600ff48
ecolvin <ecolvin@mail.csuchico.edu>
2016-10-10 16:15:57 -0700

    Add files via upload

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,37 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
-  //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
+  if(userEntered.length < 6)
+  {
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    document.getElementById("usernameGroup").classList.add("has-error");
+    document.getElementById("usernameGroup").classList.remove("has-success");
+  }
+  else
+  {
+    document.getElementById("usernameError").innerHTML="";
+    document.getElementById("usernameError").classList.add("hidden-message");
+    document.getElementById("usernameError").classList.remove("shown-message");
+    document.getElementById("usernameGroup").classList.add("has-success");
+    document.getElementById("usernameGroup").classList.remove("has-error");
+  }
 
-  //Show message that there is an error with the password...
-  document.getElementById("passwordError").innerHTML="Bad password.";
-  document.getElementById("passwordError").classList.remove("hidden-message");
-  document.getElementById("passwordError").classList.add("shown-message");
-  //Turn the password items red
-  document.getElementById("passwordGroup").classList.add("has-error");
+  if(passEntered.toLowerCase() == "password")
+  {
+    document.getElementById("passwordError").innerHTML="Bad password.";
+    document.getElementById("passwordError").classList.remove("hidden-message");
+    document.getElementById("passwordError").classList.add("shown-message");
+    document.getElementById("passwordGroup").classList.add("has-error");
+    document.getElementById("passwordGroup").classList.remove("has-success");
+  }
+  else
+  {
+    document.getElementById("passwordError").innerHTML="";
+    document.getElementById("passwordError").classList.add("hidden-message");
+    document.getElementById("passwordError").classList.remove("shown-message");
+    document.getElementById("passwordGroup").classList.add("has-success");
+    document.getElementById("passwordGroup").classList.remove("has-error")
+  }
 }
